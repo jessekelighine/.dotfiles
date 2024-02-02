0b0a2ee4a3f79be808a13f2700b81810b5add584
Jesse C. Chen <jcc@jessekelighine.com>
2024-02-03 00:36:50 +0800

    2024-02-03: clean-up and better install

--- .dotfiles-install.sh
+++ .dotfiles-install.sh
@@ -23,51 +23,52 @@
 ### Create Symlinks ###########################################################
 
 current_shell=$(echo $SHELL | xargs basename)
-[[ $current_shell == "zsh"  ]] && ln -s .config/zsh/.zshrc   .
-[[ $current_shell == "bash" ]] && ln -s .config/bash/.bashrc .
+[[ $current_shell == "zsh"  && ! -f .zshrc  ]] && ln -s .config/zsh/.zshrc   .
+[[ $current_shell == "bash" && ! -f .bashrc ]] && ln -s .config/bash/.bashrc .
 
-stow . && exit 0
+stow . && printf "$0: installed with 'stow'\n" && exit 0
+read -p "$0: symlink directly?  [Yn] " -n 1
+[[ ! $REPLY =~ ^[yY]$ ]] && printf "\n$0: aborted\n" && exit 0
 
-read -p ">>> command 'stow' not avaible, symlink directly? [Yn] " -n 1
-[[ ! $REPLY =~ ^[yY]$ ]] && printf "\n>>> aborted\n" && exit 0
+dot_locale=$(pwd)
+[ -f .zshrc  ] && ln -s "${dot_locale}/.zshrc"  "$HOME/.zshrc"
+[ -f .bashrc ] && ln -s "${dot_locale}/.bashrc" "$HOME/.bashrc"
+ln -s "${dot_locale}/.R"                "$HOME/.R"
+ln -s "${dot_locale}/.latexmkrc"        "$HOME/.latexmkrc"
+ln -s "${dot_locale}/.gitconfig"        "$HOME/.gitconfig"
+ln -s "${dot_locale}/.gnupg"            "$HOME/.gnupg"
+ln -s "${dot_locale}/.password-store"   "$HOME/.password-store"
+ln -s "${dot_locale}/.vim"              "$HOME/.vim"
+ln -s "${dot_locale}/.ssh"              "$HOME/.ssh"
+ln -s "${dot_locale}/.screenrc"         "$HOME/.screenrc"
+ln -s "${dot_locale}/.hammerspoon"      "$HOME/.hammerspoon"
+ln -s "${dot_locale}/.local/bin"        "$HOME/.local/bin"
+ln -s "${dot_locale}/.config/alacritty" "$HOME/.config/alacritty"
+ln -s "${dot_locale}/.config/b2"        "$HOME/.config/b2"
+ln -s "${dot_locale}/.config/iterm2"    "$HOME/.config/iterm2"
+ln -s "${dot_locale}/.config/mpv"       "$HOME/.config/mpv"
+ln -s "${dot_locale}/.config/nvim"      "$HOME/.config/nvim"
+ln -s "${dot_locale}/.config/octave"    "$HOME/.config/octave"
+ln -s "${dot_locale}/.config/sc-im"     "$HOME/.config/sc-im"
+ln -s "${dot_locale}/.config/skim"      "$HOME/.config/skim"
+ln -s "${dot_locale}/.config/tmux"      "$HOME/.config/tmux"
+ln -s "${dot_locale}/.config/zathura"   "$HOME/.config/zathura"
+ln -s "${dot_locale}/.config/zsh"       "$HOME/.config/zsh"
+ln -s "${dot_locale}/.config/.fzf.zsh"  "$HOME/.config/.fzf.zsh"
+ln -s "${dot_locale}/.config/.fzf.bash" "$HOME/.config/.fzf.bash"
+ln -s "${dot_locale}/.config/.bc"       "$HOME/.config/.bc"
 
-[ -f .zshrc  ] && ln -s "$HOME/.dotfiles/.zshrc"  "$HOME/.zshrc"
-[ -f .bashrc ] && ln -s "$HOME/.dotfiles/.bashrc" "$HOME/.bashrc"
-ln -s "$HOME/.dotfiles/.R"                "$HOME/.R"
-ln -s "$HOME/.dotfiles/.latexmkrc"        "$HOME/.latexmkrc"
-ln -s "$HOME/.dotfiles/.gitconfig"        "$HOME/.gitconfig"
-ln -s "$HOME/.dotfiles/.gnupg"            "$HOME/.gnupg"
-ln -s "$HOME/.dotfiles/.password-store"   "$HOME/.password-store"
-ln -s "$HOME/.dotfiles/.vim"              "$HOME/.vim"
-ln -s "$HOME/.dotfiles/.ssh"              "$HOME/.ssh"
-ln -s "$HOME/.dotfiles/.screenrc"         "$HOME/.screenrc"
-ln -s "$HOME/.dotfiles/.hammerspoon"      "$HOME/.hammerspoon"
-ln -s "$HOME/.dotfiles/.local/bin"        "$HOME/.local/bin"
-ln -s "$HOME/.dotfiles/.config/alacritty" "$HOME/.config/alacritty"
-ln -s "$HOME/.dotfiles/.config/b2"        "$HOME/.config/b2"
-ln -s "$HOME/.dotfiles/.config/iterm2"    "$HOME/.config/iterm2"
-ln -s "$HOME/.dotfiles/.config/mpv"       "$HOME/.config/mpv"
-ln -s "$HOME/.dotfiles/.config/nvim"      "$HOME/.config/nvim"
-ln -s "$HOME/.dotfiles/.config/octave"    "$HOME/.config/octave"
-ln -s "$HOME/.dotfiles/.config/sc-im"     "$HOME/.config/sc-im"
-ln -s "$HOME/.dotfiles/.config/skim"      "$HOME/.config/skim"
-ln -s "$HOME/.dotfiles/.config/tmux"      "$HOME/.config/tmux"
-ln -s "$HOME/.dotfiles/.config/zathura"   "$HOME/.config/zathura"
-ln -s "$HOME/.dotfiles/.config/zsh"       "$HOME/.config/zsh"
-ln -s "$HOME/.dotfiles/.config/.fzf.zsh"  "$HOME/.config/.fzf.zsh"
-ln -s "$HOME/.dotfiles/.config/.fzf.bash" "$HOME/.config/.fzf.bash"
-ln -s "$HOME/.dotfiles/.config/.bc"       "$HOME/.config/.bc"
-
-printf "\n>>> linked\n"
+printf "\n$0: linked\n"
 exit 0
+
+### fzf key-bindings ##########################################################
+
+$(brew --prefix)/opt/fzf/install
+mv "$HOME/.fzf.bash" "$HOME/.config/.fzf.bash"
+mv "$HOME/.fzf.zsh"  "$HOME/.config/.fzf.zsh"
 
 ### Homebrew ##################################################################
 
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle install
-
-## fzf ( to install key-bindings )
-$(brew --prefix)/opt/fzf/install
-# mv -i "$HOME/.fzf.bash" "$HOME/.dotfiles/.fzf.bash"
-# mv -i "$HOME/.fzf.zsh"  "$HOME/.dotfiles/.fzf.zsh"
